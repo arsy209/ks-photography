dfeae9fe5760bdee4a813a621c7ec0b823a401af
Arshdeep Singh <singh_arsy@yahoo.com>
2018-10-09 11:22:12 -0400

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root "home#index"
 
   get "/services" ,to: "services#index"
+  get "/contacts", to: "contacts#contact"
+  post "email_form", to: "contacts#create"
+  get "/about_us" ,to: "home#about_us"
+  get "/portfolio" ,to: "portfolio#index"
 end
