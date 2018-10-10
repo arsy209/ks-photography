29841ae7843240ecac407abf1c2faf869ae1403b
Arshdeep Singh <singh_arsy@yahoo.com>
2018-10-09 23:36:04 -0400

    add portolio title react

---
+++ app/assets/javascripts/components/portfolio/title.jsx
@@ -0,0 +1,10 @@
+class PortfolioTitle extends React.Component {
+  render() {
+    return (
+    <div>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.paragraph}</p>
+    </div>
+    )
+  }
+}
