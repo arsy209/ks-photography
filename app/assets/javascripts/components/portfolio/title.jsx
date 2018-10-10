fd9b6567dde6f294dfd44e11745b4fb36ba54219
Arshdeep Singh <singh_arsy@yahoo.com>
2018-10-10 09:57:57 -0400

    move divs to react folder

--- app/assets/javascripts/components/portfolio/title.jsx
+++ app/assets/javascripts/components/portfolio/title.jsx
@@ -2,8 +2,14 @@ class PortfolioTitle extends React.Component {
   render() {
     return (
     <div>
-        <h1>{this.props.title}</h1>
-        <p>{this.props.paragraph}</p>
+      <div className="row">
+        <div className="col-md-12 text-center">
+          <article className="sa">
+            <h1>{this.props.title}</h1>
+            <p>{this.props.paragraph}</p>
+          </article>
+          </div>
+      </div>
     </div>
     )
   }
