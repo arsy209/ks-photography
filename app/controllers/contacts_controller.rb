e8483851403a6a34e77816f3c2c4a7cc26a761f4
Arshdeep Singh <singh_arsy@yahoo.com>
2018-10-17 11:41:12 -0400

    add contact controller methods

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
+
   def contact
   end
+
+  def create
+    data = Contact.new(contact_params)
+    ContactMailer.testing(data).deliver_now
+    redirect_to root_path
+
+  end
+
+  private
+
+  def contact_params
+    params.require(:anything).permit(:name, :email, :phone_number, :body)
+  end
+
 end
