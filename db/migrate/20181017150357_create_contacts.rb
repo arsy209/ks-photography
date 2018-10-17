bd73a1ce0246d6a968e2bb03762366d95a543016
Arshdeep Singh <singh_arsy@yahoo.com>
2018-10-17 11:09:55 -0400

    create contacts migration

---
+++ db/migrate/20181017150357_create_contacts.rb
@@ -0,0 +1,10 @@
+class CreateContacts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.string :phone_number
+      t.string :body
+    end
+  end
+end
