8ee04da39dd97c1fc5d67e9f28fe9d44951cc92f
Arshdeep Singh <singh_arsy@yahoo.com>
2018-10-18 17:20:15 -0400

    render react  header

---
+++ app/assets/javascripts/components/about/header.jsx
@@ -0,0 +1,16 @@
+class Header extends React.Component {
+  render() {
+    return (
+    <div>
+
+      <div className="col-md-8 col-md-offset-2">
+        <div className="align">
+          <h1 className="h1 light">{this.props.title}</h1>
+        </div>
+      </div>
+
+
+    </div>
+    )
+  }
+}
